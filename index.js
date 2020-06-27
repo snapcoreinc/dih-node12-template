59c1a0372dde3d01232dce6ef0de05a9bce93cf8
Olivier Refalo <orefalo@snapcore.com>
2020-06-27 12:11:33 +0400

    ch

--- index.js
+++ index.js
@@ -121,6 +121,11 @@ const fnHandler = (req, res) => {
     else {
     }
 };
+async function allIsGood() {
+    return true;
+}
+app.get("/metrics", service_tools_1.middleware.express.prometheusMetrics());
+app.get("/_/health", service_tools_1.middleware.express.healthCheck([allIsGood]));
 app.post("/*", fnHandler);
 app.get("/*", fnHandler);
 app.patch("/*", fnHandler);
